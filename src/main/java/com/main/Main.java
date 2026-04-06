424e48ff170039bacb592ab858178c069b3fdbbf
Ashish Madheshiya <akmadheshiya@icloud.com>
2026-04-06 07:28:49 +0530

    Merge Sort

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,18 +1,17 @@
 package com.main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.sorting.*;
+
+import java.util.Arrays;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        int[] array = DataGenerators.generatePositiveIntArray(20,10,99);
+        System.out.println(Arrays.toString(array));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        Sort sorting = new Sort();
+        sorting.mergeSort(array, 0, array.length-1);
+        System.out.println(Arrays.toString(array));
     }
 }
 
